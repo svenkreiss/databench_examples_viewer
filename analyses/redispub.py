8e159a69e5b8035db2639fad1dfe06d2a2456b34
Sven Kreiss <sk@svenkreiss.com>
2014-06-15 23:20:09 -0400

    Make redis pubsub demos suitable for public live demos. Add redis requirement.

---
+++ analyses/redispub.py
@@ -0,0 +1,39 @@
+"""Testing publishing to a redis channel."""
+
+import os
+import redis
+import urlparse
+
+import databench
+
+
+ANALYSIS = databench.Analysis('redispub', __name__)
+ANALYSIS.description = __doc__
+# ANALYSIS.thumbnail = 'redispub.png'
+
+def redis_creator():
+    """Checks enironment for certatin redis providers and creates
+    a redis client."""
+
+    rediscloudenv = os.environ.get('REDISCLOUD_URL')
+    if rediscloudenv:
+        url = urlparse.urlparse(rediscloudenv)
+        r = redis.Redis(host=url.hostname, port=url.port, password=url.password)
+    else:
+        r = redis.StrictRedis()
+
+    return r
+
+@ANALYSIS.signals.on('connect')
+def onconnect():
+    """Run as soon as a browser connects to this."""
+    ANALYSIS.signals.emit('log', {'action': 'backend started'})
+
+    redis_client = redis_creator()
+
+    @ANALYSIS.signals.on('stats')
+    def onstats(msg):
+        """Listens for new messages from frontend and pushes to
+        redis channel."""
+
+        redis_client.publish('someStatsProvider', msg)
